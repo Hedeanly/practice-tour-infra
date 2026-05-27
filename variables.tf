53a6d5e2c7d09730bc6ff3b4a67f7d30742fce28
Hedeanly <peungpognleap@gmail.com>
2026-05-27 13:54:13 +0700

    initial project setup: terraform, hello-service, gitignore

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "project_id" {
+  description = "to identify project"
+  type        = string
+}
+
+variable "region" {
+  description = "resource region"
+  type        = string
+  default     = "asia-southeast1"
+}
